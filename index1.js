19179500359160237c2cf0b5e7bbd2ea847ad39b
rogercoaquirajusto <117381061+rogercoaquirajusto@users.noreply.github.com>
2022-11-24 17:06:25 -0500

    archivo_index1.js
    archivo_index.js

---
+++ index1.js
@@ -0,0 +1,23 @@
+// En estas primeras preguntas, reemplaza `null` por la respuesta
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = "roger";
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 20;
+// Crea una variable booleana:
+const nuevoBool = false;
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - 5 === 5;
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = 10 * 4 === 40 ;
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5=== 1;
+// No modificar nada debajo de esta línea
+// --------------------------------
+module.exports = {
+nuevaString,
+nuevoNum,
+nuevoBool,
+nuevaResta,
+nuevaMultiplicacion,
+nuevoModulo,
+}
